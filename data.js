f0387cea02aa93ac1735bda545996fa9df26b43a
ConstructAware <lp.lencone@gmail.com>
2022-03-24 19:36:01 -0300

    learning query string params
    also renamed express-params.js to express-routeParams.js, just to clarify which one I was learning before

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ const products = [
     image:
       'https://dl.airtable.com/.attachments/6ac7f7b55d505057317534722e5a9f03/9183491e/product-3.jpg',
     price: 39.95,
-    desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+    desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ const products = [
     image:
       'https://dl.airtable.com/.attachments/da5e17fd71f50578d525dd5f596e407e/d5e88ac8/product-2.jpg',
     price: 29.98,
-    desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+    desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ const products = [
     image:
       'https://dl.airtable.com/.attachments/05ecddf7ac8d581ecc3f7922415e7907/a4242abc/product-1.jpeg',
     price: 10.99,
-    desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+    desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`
   },
   {
     id: 4,
@@ -29,14 +29,22 @@ const products = [
     image:
       'https://dl.airtable.com/.attachments/3245c726ee77d73702ba8c3310639727/f000842b/product-5.jpg',
     price: 9.99,
-    desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+    desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`
   },
+  {
+    id: 5,
+    name: 'amber sofa',
+    image:
+      'https://dl.airtable.com/.attachments/3245c726ee77d73702ba8c3310639727/f000842b/product-4.jpg',
+    price: 12.99,
+    desc: `lorem ipslum market cap fan struggle average make it better data javacript description productive iron shott triple inferno shudder morbid trickle sup man never mind`
+  }
 ]
 const people = [
   { id: 1, name: 'john' },
   { id: 2, name: 'peter' },
   { id: 3, name: 'susan' },
   { id: 4, name: 'anna' },
-  { id: 5, name: 'emma' },
+  { id: 5, name: 'emma' }
 ]
 module.exports = { products, people }
